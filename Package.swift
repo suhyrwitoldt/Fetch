81981dec6ef92d9947b8df3f0d0453201f48b8c6
suhyrwitoldt <5180138+suhyrwitoldt@users.noreply.github.com>
2025-05-24 07:50:08 -0300

    specify min version support

--- Package.swift
+++ Package.swift
@@ -8,6 +8,8 @@ let package = Package(
     platforms: [
       .iOS(.v13),
       .macOS(.v10_15),
+      .tvOS(.v13),
+      .watchOS(.v6),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
